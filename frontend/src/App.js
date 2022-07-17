1fcfb4928f587af61dee998f27ce9e7544b1ebc0
alex-budko <xalex.budko@gmail.com>
2022-07-16 22:57:44 -0400

    added wildfire trackers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 
 import "./styles/styles.css";
 
 function App() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
-  });
+  const [wildfireCoordinates, setWildfireCoordinates] = useState([]);
+  const [ready, setReady] = useState(false);
 
-  if (!isLoaded) {
-    return <div>Loading...</div>;
-  }
+  const getData = async () => {
+    setWildfireCoordinates([]);
+    await fetch("https://eonet.gsfc.nasa.gov/api/v3/events")
+      .then((response) => response.json())
+      .then((data) => {
+        for (let i = 0; i < data.events.length; i++) {
+          if (data.events[i].categories[0].id === "wildfires") {
+            setWildfireCoordinates((wildfireCoordinates) => [
+              ...wildfireCoordinates,
+              {
+                lat: Math.floor(data.events[i].geometry[0].coordinates[1]),
+                lng: Math.floor(data.events[i].geometry[0].coordinates[0]),
+              },
+            ]);
+          }
+        }
+      });
+      setReady(true)
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useLoadScript({
+    googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
+  })
 
   return (
     <>
-      <GoogleMap
-        zoom={10}
-        center={{ lat: 44, lng: -80 }}
-        mapContainerClassName="map-container"
-      >
-        <Marker position={{ lat: 44, lng: -80 }}></Marker>
-      </GoogleMap>
+      <button onClick={() => getData()}>Fetch Data</button>
+      <button onClick={() => console.log(wildfireCoordinates)}>Fetch Data</button>
+
+      {ready && (
+          <GoogleMap
+            onClick={() => console.log("clicked")}
+            zoom={10}
+            center={{ lat: 44, lng: -80 }}
+            mapContainerClassName="map-container"
+          >
+            {wildfireCoordinates.map((wildfireCoordinate, i) => {
+              return (
+                <Marker
+                  key={i}
+                  position={{
+                    lat: wildfireCoordinate["lat"],
+                    lng: wildfireCoordinate["lng"],
+                  }}
+                />
+              );
+            })}
+          </GoogleMap>
+      )}
     </>
   );
 }
