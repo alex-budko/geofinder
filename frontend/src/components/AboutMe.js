2e5cde9fe4f7ef0059c794c6816ff6c1d76dc00d
alex-budko <xalex.budko@gmail.com>
2022-07-27 23:11:24 -0400

    weather api

--- frontend/src/components/AboutMe.js
+++ frontend/src/components/AboutMe.js
@@ -24,26 +24,6 @@ export default function AboutMe() {
         rounded={"md"}
         overflow={"hidden"}
       >
-        <Image
-          h={"120px"}
-          w={"full"}
-          src={
-            "https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-          }
-          objectFit={"cover"}
-        />
-        <Flex justify={"center"} mt={-12}>
-          <Avatar
-            size={"xl"}
-            src={
-              "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-            }
-            alt={"Author"}
-            css={{
-              border: "2px solid white",
-            }}
-          />
-        </Flex>
 
         <Box p={6}>
           <Stack spacing={0} align={"center"} mb={5}>
@@ -55,7 +35,7 @@ export default function AboutMe() {
 
           <Stack direction={"row"} justify={"center"} spacing={12}>
             <Stack spacing={0} align={"center"}>
-              <Text fontSize={"sm"} color={"gray.500"}>
+              <Text fontSize={"sm"} color={"gray.200"}>
                 Hey, my name is Alex and I am an undegrad at UPenn!
                 <br /> <br />I developed this open-source app in hopes that it
                 would make it easier for people to find potential danger alerts,
--- frontend/src/utils/getWeatherData.js
+++ frontend/src/utils/getWeatherData.js
@@ -1,3 +1,4 @@
 export const getWeatherData = async (lat, lng) => {
+  await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=17.38,78.48`)
   };
   
