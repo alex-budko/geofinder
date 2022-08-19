b8838e80d0aa7f9bc124822b1cd5ea652e9e7ef5
alex-budko <xalex.budko@gmail.com>
2022-08-19 15:46:51 -0400

    restructuring

--- backend/settings.py
+++ backend/settings.py
@@ -20,9 +20,9 @@ CELERY_BROKER_URL = 'redis://localhost:6379'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['geofinders.herokuapp.com/', '127.0.0.1']
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
+import axios from 'axios'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,9 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
