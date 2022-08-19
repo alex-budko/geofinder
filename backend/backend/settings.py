c8e74bca4390b77b4c4a25d902a6f8a9fd1b24f6
alex-budko <xalex.budko@gmail.com>
2022-08-19 19:32:09 -0400

    smpt email

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", '')
 #  env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
